f20c850deeacae0bb7fa696f7e31b3bd8756bc1d
NishiOwO <nishi@nishi.boats>
2025-11-22 17:50:41 +0900

    better

--- cmake/MDEPkgConfig.cmake
+++ cmake/MDEPkgConfig.cmake
@@ -7,10 +7,20 @@ macro(pkg_config_add dest name)
 		PRIVATE
 		${${name}_INCLUDE_DIRS}
 	)
+	target_compile_options(
+		${dest}
+		PRIVATE
+		${${name}_CFLAGS_OTHER}
+	)
 	target_link_directories(
 		${dest}
 		PRIVATE
 		${${name}_LIBRARY_DIRS}
+	)
+	target_link_options(
+		${dest}
+		PRIVATE
+		${${name}_LDFLAGS_OTHER}
 	)
 	target_link_libraries(
 		${dest}
